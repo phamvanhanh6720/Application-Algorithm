24ad59067015b8d2c6f1d7a28672faee52324663
phamvanhanh6720 <phamvanhanh05808@gmail.com>
2021-04-22 23:36:24 +0700

    gold_mining traning 5 is soldved by vet can

--- Training5/max_even_subsequence.cpp
+++ Training5/max_even_subsequence.cpp
@@ -2,45 +2,48 @@
 
 using namespace std;
 int n;
-const int MAX = 1e6 + 1;
-int A[MAX];
-long long prefix_sum[MAX] = {0};
+const int N = 1e6 + 1;
+int a[N];
+long long pre_sum[N];
 
 int main()
 {
-
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     cin >> n;
+    pre_sum[0] = 0;
     for (int i = 1; i <= n; i++)
     {
-        cin >> A[i];
-    }
-    prefix_sum[1] = A[1];
-    for (int i = 2; i <= n; i++)
-    {
-        prefix_sum[i] = prefix_sum[i - 1] + A[i];
+        cin >> a[i];
+        pre_sum[i] = pre_sum[i - 1] + a[i];
     }
 
-    long long max_odd_r = -(1e15);
-    long long max_even_r = -(1e15);
-    long long min_odd_l = 1e15;
-    long long  min_even_l = 1e15;
+    long long res = -1e15;
+    long long min_odd = (long long)1e15 + 1;
+    long long min_even = (long long)1e15;
 
     for (int i = 1; i <= n; i++)
     {
-        long long temp = A[i];
-        if (temp % 2 ==0 ){
-            max_even_r = max(max_even_r, temp);
-            min_even_l = min(min_even_l, temp);
+        long long sum = pre_sum[i];
+        if (sum % 2)
+        {
+            sum -= min_odd;
         }
-        else{
-            max_odd_r = max(max_odd_r , temp);
-            min_odd_l = min(min_odd_l, temp);
+        else
+        {
+            sum -= min_even;
         }
-
-        
-        
+        res = max(res, sum);
+        if (pre_sum[i] % 2)
+        {
+            min_odd = min(min_odd, pre_sum[i]);
+        }
+        else
+        {
+            min_even = min(min_even, pre_sum[i]);
+        }
     }
-    long long res1 = (max_odd_r - min_odd_l);
-    long long res2 = (max_even_r - min_even_l);
-    cout << max(res1, res2);
+    cout << res << endl;
+
+    return 0;
 }
